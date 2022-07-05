452fdea9939cdcbf3f93dd3576d6217fd02b1d8c
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-05 17:20:30 +0300

    Добавил тестовую диаграмку на фронт

--- services/client/src/App.vue
+++ services/client/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/diagram">Diagram</RouterLink>
       </nav>
     </div>
   </header>
--- services/client/src/main.ts
+++ services/client/src/main.ts
@@ -9,4 +9,13 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.config.globalProperties.$apexcharts = ApexCharts;
+
+// Add this when into a TypeScript codebase
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $apexcharts: typeof ApexCharts;
+  }
+}
+
 app.mount('#app')
