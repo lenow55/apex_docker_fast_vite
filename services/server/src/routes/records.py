a097168605f83db4a2653c8c8118619a3bbcc1df
Новиков Илья2 <nov.ilucha@yandex.ru>
2022-07-11 01:59:53 +0300

    путь для создания записи

---
+++ services/server/src/routes/records.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException
+
+import src.crud.records as crud
+from schemas.records import RecordSchema
+
+
+router = APIRouter()
+
+# будем сразу делать запрос на все схемы и
+# возвращать данные
+
+
+@router.post(
+    "/records", response_model=None
+)
+async def create_record(
+    record: RecordSchema
+):
+    return await crud.create_record(record)
