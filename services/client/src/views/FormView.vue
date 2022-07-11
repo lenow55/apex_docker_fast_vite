4f68c8ea85654f6b6e5d1c9d71fb63322961dd67
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-11 22:16:02 +0300

    добавил форму для записей

---
+++ services/client/src/views/FormView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BaseCheckboxVue from '@/components/form-components/BaseCheckbox.vue'
+import BaseInputVue from '@/components/form-components/BaseInput.vue'
+
+export default defineComponent({
+  components: {
+    baseinput: BaseInputVue,
+    basecheckbox: BaseCheckboxVue,
+  },
+  methods: {
+    sendForm() {
+      console.log("sendForm");
+    }
+  }
+})
+</script>
+
+<template>
+  <form @submit.prevent="sendForm"></form>
+</template>
