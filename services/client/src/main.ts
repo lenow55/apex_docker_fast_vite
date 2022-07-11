1fbf8868baa550bbf75bdd93e28c6c3684141dfa
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-11 15:06:11 +0300

    конфигурация аксиоса

--- services/client/src/main.ts
+++ services/client/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
+
+// axios.defaults.baseURL = "/api"
+axios.defaults.baseURL = "localhost:5000"
 
 const app = createApp(App)
 
