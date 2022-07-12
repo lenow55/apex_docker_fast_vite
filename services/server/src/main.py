7c668e493d88e44227dd0d7d6bf6eb639b3ecdb2
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-12 18:08:16 +0300

    теперь работает бд

--- services/server/src/main.py
+++ services/server/src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routes import diagrams, records
 
+from tortoise import Tortoise
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
+
+Tortoise.init_models(["src.database.models"], "models")
 
 app = FastAPI()
 
@@ -12,9 +16,33 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+from src.routes import diagrams, records
 app.include_router(diagrams.router)
 app.include_router(records.router)
 
 @app.get("/")
 def test():
     return "TEST"
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
+
+debug = True
+print(debug)
+if debug == True:
+    from tortoise.log import logger, db_client_logger
+    import logging
+    import sys
+    print("DEBUG MODE")
+    fmt = logging.Formatter(
+        fmt="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.DEBUG)
+    sh.setFormatter(fmt)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(sh)
+    db_client_logger.setLevel(logging.DEBUG)
+    db_client_logger.addHandler(sh)
