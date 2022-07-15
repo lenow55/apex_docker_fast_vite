5ea0fcdc1fdabb8fbd2d8c5ab07164331e7c4730
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-15 08:49:48 +0300

    поменял диаграмки и события работает

--- services/client/src/views/ChartView.vue
+++ services/client/src/views/ChartView.vue
@@ -1,49 +1,51 @@
 <template>
-  <div class="app">
-    <apexcharts width="550" type="bar" :options="chartOptions" :series="series"></apexcharts>
-  </div>
+  <apexchart v-if="options && series" type="line" height="350" :options="options" :series="series" @click="click"
+    @legendClick="legendClick" @markerClick="markerClick" @selection="selection"
+    @dataPointSelection="dataPointSelection">
+  </apexchart>
 </template>
 
-<script lang="ts">
-import VueApexCharts from 'vue3-apexcharts'
+<script lang="ts">import { defineComponent } from "vue";
+import VueApexCharts from "vue3-apexcharts";
 
-export default {
-  name: 'Chart',
+export default defineComponent({
   components: {
-    apexcharts: VueApexCharts,
+    apexchart: VueApexCharts,
   },
-  data: function () {
+  data() {
     return {
-      chartOptions: {
+      options: {
         chart: {
-          type: 'bar',
-          id: 'vuechart-example',
-          events: {
-            click: function(event: any) {
-              console.log(event.target)
-            }
-          }
+          id: 'vuechart-example'
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          categories: [1991, 1992, 1993],
         },
       },
-      series: [{
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 81]
-      }]
-    }
-  }
-}
+      series: [
+        {
+          name: 'series-1',
+          data: [30, 40, 45],
+        },
+      ],
+    };
+  },
+  methods: {
+    click(event: any, chartContext: any, config: any) {
+      console.log('click', event, chartContext, config);
+    },
+    legendClick(chartContext: any, seriesIndex: any, config: any) {
+      console.log('legendClick', chartContext, seriesIndex, config);
+    },
+    markerClick(event: any, chartContext: any, { seriesIndex, dataPointIndex, config }: any) {
+      console.log('markerClick', event, chartContext, seriesIndex, dataPointIndex, config);
+    },
+    selection(chartContext: any, { xaxis, yaxis }: any) {
+      console.log('selection', chartContext, xaxis, yaxis);
+    },
+    dataPointSelection(event: any, chartContext: any, config: any) {
+      console.log('dataPointSelection', event, chartContext, config);
+    },
+  },
+})
 </script>
-<style scoped>
-button {
-  background: #26E6A4;
-  border: 0;
-  font-size: 16px;
-  color: '#fff';
-  padding: 10px;
-  margin-left: 28px;
-}
-</style>
-
--- services/client/src/views/DiagramView.vue
+++ services/client/src/views/DiagramView.vue
@@ -1,9 +1,7 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import VueApexCharts from 'vue3-apexcharts'
-import type { ApexOptions } from 'apexcharts'
-
-
+import { defineComponent, ref } from "vue";
+import VueApexCharts from "vue3-apexcharts";
+import type { ApexOptions } from "apexcharts";
 
 export default defineComponent({
   components: {
@@ -12,31 +10,56 @@ export default defineComponent({
   setup() {
     const options = ref<ApexOptions>({
       chart: {
-        type: 'donut',
-        id: 'vuechart-example',
-        events: {
-          markerClick: function(event: any, ...rest): void {
-            console.log("event")
-          }
-        }
+        type: "donut",
+        id: "vuechart-example",
       },
-    })
-    const series = ref([30, 40, 45, 50, 49, 60, 70, 81])
+    });
+    const series = ref([30, 40, 45, 50, 49, 60, 70, 81]);
+
+    function click(event: any, chartContext: any, config: any) {
+      console.log("click", event, chartContext, config);
+    }
+    function legendClick(chartContext: any, seriesIndex: any, config: any) {
+      console.log("legendClick", chartContext, seriesIndex, config);
+    }
+    function markerClick(
+      event: any,
+      chartContext: any,
+      { seriesIndex, dataPointIndex, config }: any
+    ) {
+      console.log(
+        "markerClick",
+        event,
+        chartContext,
+        seriesIndex,
+        dataPointIndex,
+        config
+      );
+    }
+    function selection(chartContext: any, { xaxis, yaxis }: any) {
+      console.log("selection", chartContext, xaxis, yaxis);
+    }
+    function dataPointSelection(event: any, chartContext: any, config: any) {
+      console.log("dataPointSelection", event, chartContext, config);
+    }
 
     return {
       options,
       series,
-    }
-  }
-})
-
-
-
+      click,
+      legendClick,
+      markerClick,
+      selection,
+      dataPointSelection,
+    };
+  },
+});
 </script>
 
 <template>
   <div>
-    <apexchart :options="options" :series="series" @markerClick="options.chart.events.markerClick">
+    <apexchart :options="options" :series="series" @click="click" @legendClick="legendClick" @markerClick="markerClick"
+      @selection="selection" @dataPointSelection="dataPointSelection">
     </apexchart>
   </div>
 </template>
