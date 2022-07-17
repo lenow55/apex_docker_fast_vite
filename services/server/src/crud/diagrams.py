1bc0b81ab24dbf33813bed3ab11e4275d8b64c3a
Новиков Илья <nov.ilucha@yandex.ru>
2022-07-17 15:26:05 +0300

    добавил полное выведение данных фильтация не работает

--- services/server/src/crud/diagrams.py
+++ services/server/src/crud/diagrams.py
@@ -13,27 +13,22 @@ from src.validator.validator import Validator
 
 
 async def get_diagrams(rules: List[DiagramRule]):  # Тут делаем запрос к базе данных
-    data_diagrams = []
     validator = Validator()
-    #d_var_1 = DiagramData(
-    #    id=1, name="diagram1", series=[],
-    #    cat_ids=[],
-    #    categories=["positive", "negative"])
+    data_diagrams = []
     filters_list = []
     filters_list = [Q(val_1=True), Q(val_2=False)]
     try:
-        for field in CategoriesRecordSchema.__fields__.keys():
+        for column in validator.get_fields():
             data_diagrams.append(
                 await Records
                 .filter(Q(*filters_list, join_type='AND'))
-                .annotate(count=Count(field))
-                .group_by(field)
-                .values(field, "count"))
+                .annotate(count=Count(column))
+                .group_by(column)
+                .values(column, "count"))
     except Exception as ex:
         print(ex)
     print(data_diagrams)
     out_diagrams_data: List[DiagramData] = []
-    i = 0
     for diagram in data_diagrams:
         out_diagrams_data.append(validator.serialise(diagram))
     return out_diagrams_data
--- services/server/src/validator/validator.py
+++ services/server/src/validator/validator.py
@@ -1,28 +1,35 @@
 from typing import List
-from src.schemas.diagram import DiagramData, DiagramRule
+
+from tortoise.expressions import Q
+from src.schemas.diagram import Data, DiagramData, DiagramRule
+
 
 class column2id():
     id: int
     name: str
-    description: str
     db_name: str
+    description: str
+
     def __init__(self, id, name, db_name, description):
         self.id = id
         self.name = name
         self.db_name = db_name
         self.description = description
 
+
 class cat2id():
     id: int
     name: str
+
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
 
 class Validator():
-    column_to_id = []
-    cat_to_id = []
+    column_to_id: List[column2id]
+    cat_to_id: List[cat2id]
+
     def __init__(self):
         self.column_to_id.append(column2id(1, "значение 1", "val_1", "просто булево значение"))
         self.column_to_id.append(column2id(2, "значение 2", "val_2", "просто булево значение"))
@@ -36,12 +43,25 @@ class Validator():
     def serialise(self, diagram):
         name = list(diagram[0])[0]
         dt = [i for i in self.column_to_id if i.name == name][0]
-        data = []
+        data: List[Data] = []
         for rec in diagram:
+            name_categorie = str(rec[name])
+            count = rec['count']
+            cat = [i for i in self.cat_to_id if i.name == name_categorie][0]
+            data.append(Data(id=cat.id, name=name_categorie, count=count))
 
-        
-            return DiagramData(
-                id=i, name=list(diagram[0])[0],
-                series=[rec['count'] for rec in diagram],
-                cat_ids=[1, 2],
-                categories=["first", "second"]))
+        return DiagramData(
+            id=dt.id, name=dt.name, description=dt.description, data=data
+        )
+
+    def get_fields(self):
+        return (i.db_name for i in self.column_to_id)
+
+    def get_filters(self, rules: List[DiagramRule]) -> List[Q]:
+        query: List[Q] = []
+        for rule in rules:
+            dt = [i for i in self.column_to_id if i.id == rule.id_diagram][0]
+            query.append(Q({dt.db_name: True}))
+
+
+
