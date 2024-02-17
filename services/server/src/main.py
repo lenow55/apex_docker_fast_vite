96b12e52fa71c8734d7ce065cfea701e84f8d9b7
Новиков Илья <nov.ilucha@yandex.ru>
2024-02-17 08:25:58 +0300

    форматирование и организация импортов

--- services/server/src/main.py
+++ services/server/src/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from tortoise import Tortoise
-from src.database.register import register_tortoise
+
 from src.database.config import TORTOISE_ORM
-from src.env import get_url, get_root, get_debug
+from src.database.register import register_tortoise
+from src.env import get_debug, get_root, get_url
 
 Tortoise.init_models(["src.database.models"], "models")
 
@@ -19,22 +19,27 @@ app.add_middleware(
 )
 
 from src.routes import diagrams, records
+
 app.include_router(diagrams.router)
 app.include_router(records.router)
+
 
 @app.get("/")
 def test():
     return "TEST"
+
 
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 debug = get_debug()
 print(debug)
-if debug == True:
-    from tortoise.log import logger, db_client_logger
+if debug is True:
     import logging
     import sys
+
+    from tortoise.log import db_client_logger, logger
+
     print("DEBUG MODE")
     fmt = logging.Formatter(
         fmt="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
